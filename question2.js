be251b5b8ea6c22e2389d162736c2fc8c391cde7
YY208@ccs.local <YY208@ccs.local>
2025-03-10 20:56:48 +0800

    Add question2.js

---
+++ question2.js
@@ -0,0 +1,6 @@
+function filterEvenNumbers(numbers) {  
+    return numbers.filter(number => number % 2 === 0);  
+  }  
+
+const result = filterEvenNumbers([1, 2, 3, 4, 5, 6]);  
+console.log(result); // Output: [2, 4, 6]  
